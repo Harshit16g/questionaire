11358f77b95a653a17362776411c60222dda5682
HARSHIT <73606353+Harshit16g@users.noreply.github.com>
2024-12-21 10:28:32 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import { type ClassValue, clsx } from "clsx"
-=======
+
 import { clsx, type ClassValue } from "clsx"
->>>>>>> a2d4f62 (	modified:   .gitignore)
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> a2d4f62 (	modified:   .gitignore)
